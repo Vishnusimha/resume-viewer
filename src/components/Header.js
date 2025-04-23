502ba07b0b35dcc0aff0f8ac48c3d3103a07670c
Vishnu Simha <vishnusimha01@gmail.com>
2025-04-23 14:59:25 +0100

    minor updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,23 +6,37 @@ const Header = ({ name, title, contact }) => {
       <h1 className="name">{name}</h1>
       <h2 className="title">{title}</h2>
       <div className="contact-info">
-        <p>{contact.university}</p>
-        <p>
-          Email: <a href={`mailto:${contact.email}`}>{contact.email}</a>
-        </p>
-        <p>
-          LinkedIn:{" "}
-          <a href={contact.linkedin} target="_blank" rel="noopener noreferrer">
-            {contact.linkedin}
-          </a>
-        </p>
-        <p>
-          Github:{" "}
-          <a href={contact.github} target="_blank" rel="noopener noreferrer">
-            {contact.github}
-          </a>
-        </p>
-        <p>Phone: {contact.phone}</p>
+        {contact.university && <p>{contact.university}</p>}
+
+        {contact.email && (
+          <p>
+            Email: <a href={`mailto:${contact.email}`}>{contact.email}</a>
+          </p>
+        )}
+
+        {contact.linkedin && (
+          <p>
+            LinkedIn:{" "}
+            <a
+              href={contact.linkedin}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {contact.linkedin}
+            </a>
+          </p>
+        )}
+
+        {contact.github && (
+          <p>
+            GitHub:{" "}
+            <a href={contact.github} target="_blank" rel="noopener noreferrer">
+              {contact.github}
+            </a>
+          </p>
+        )}
+
+        {contact.phone && <p>Phone: {contact.phone}</p>}
       </div>
     </header>
   );
